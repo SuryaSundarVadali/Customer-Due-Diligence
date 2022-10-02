a26bb050643e1963f8f4cd8c3ac69eebdd67df84
Yathin Prakash K <yathinprakash017@gmail.com>
2022-10-02 13:07:57 +0530

    Create insertData.py

---
+++ insertData.py
@@ -0,0 +1,42 @@
+import mysql.connector
+
+# Convert digital data to binary format
+def convertToBinaryData(filename):
+    with open(filename, 'rb') as file:
+        binaryData = file.read()
+    return binaryData
+
+def insertBLOB(aadhar_number, pan_number, first_name, middle_name, last_name, father_name, mother_name, dob, profile_image):
+    print("Inserting data into MySQL")
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                             port='3306',
+                                             user='root',
+                                             password='password',
+                                             database='test_schema')
+
+        cursor = connection.cursor()
+        sql_insert_query = """ INSERT INTO new_table
+                          (aadhar_number, pan_number, first_name, middle_name, last_name, father_name, mother_name, dob, profile_image) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+
+        profileImage = convertToBinaryData(profile_image)
+
+        # Convert data into tuple format
+        insert_tuple = (aadhar_number, pan_number, first_name, middle_name,
+                        last_name, father_name, mother_name, dob, profileImage)
+        result = cursor.execute(sql_insert_query, insert_tuple)
+        connection.commit()
+        print("Data inserted to MySQL successfully", result)
+
+    except mysql.connector.Error as error:
+        print("Failed inserting data into MySQL {}".format(error))
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+
+insertBLOB("123456781234", "ABC1234", "Yathin", "Prakash", "Kethepalli", "Sarath",
+           "Sujatha", "2002-08-17", "D:\Projects\IdentityVerification\yathin_prakash.png")
